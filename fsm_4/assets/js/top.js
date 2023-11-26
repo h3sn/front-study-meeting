c0eb7f732ee20adf6997f6de9b86f68bc2987937
Shun Nakano <greenpeas.y.man@gmail.com>
2023-11-26 21:24:54 +0900

    画像にライセンスを付与する処理を適用

--- fsm_4/assets/js/top.js
+++ fsm_4/assets/js/top.js
@@ -1,6 +1,5 @@
-
 // GARALLY
-const GARALLY_SEARCH_WORD = 'croissant';
+const GARALLY_SEARCH_WORD = 'bread';
 const GARALLY_PER_PAGE = 10;
 const GARALLY_QUERY = new URLSearchParams({ ...QUERY, ...{ per_page: `${GARALLY_PER_PAGE}`, text: `${GARALLY_SEARCH_WORD}` } });
 const GARALLY_URL = `${PATH}?${GARALLY_QUERY}`;
@@ -9,34 +8,50 @@ const createProductGarally = async () => {
     const SELECTOR_THUMBNAILS = '.js-product-garally__thumbnails';
     const mainTarget = document.querySelector(`${SELECTOR_MAIN}`);
     const thumbnailsTarget = document.querySelector(`${SELECTOR_THUMBNAILS}`);
-    const domParser = new DOMParser();
-    const photos = await fetchFlickerPhotos(GARALLY_URL);
-    const mainImg = domParser.parseFromString(`<img src="${photos[0].url_h}" alt="${photos[0].title}">`, 'text/html').querySelector('img');
-    // const thumbnailsImg = photos.map(photo => `<img src="${photo.url_h}" alt="${photo.title}">`).join('');
-    const thumbnailsImg = domParser.parseFromString(photos.map(photo => `<img src="${photo.url_h}">`).join(''), 'text/html').querySelectorAll('img');
-    thumbnailsImg.forEach(img => {
-        img.addEventListener('click', () => {
-            const src = img.getAttribute('src');
-            mainImg.setAttribute('src', src);
+    const photos = await fetchFlickrPhotos(GARALLY_URL);
+    const mainImg = createLicensedImageTag({
+        url: `${photos[0].url_o}`, 
+        alt: `${photos[0].title}`,
+        owner: `${photos[0].owner}`,
+        id: `${photos[0].id}`
+    });
+    const thumbnailsImg = photos.map(photo => createLicensedImageTag({
+        url: `${photo.url_o}`,
+        alt: `${photo.title}`,
+        owner: `${photo.owner}`,
+        id: `${photo.id}`
+    }));
+    thumbnailsImg.forEach(div => {
+        const img = div.querySelector('img');
+        img.addEventListener('click', (e) => {
+            const src = e.target.getAttribute('src');
+            const img = mainImg.querySelector('img');
+            img.setAttribute('src', src);
         });
-        thumbnailsTarget.appendChild(img);
+        thumbnailsTarget.appendChild(div);
     });
     mainTarget.appendChild(mainImg);
 };
 
 // RECOMEND
 const RECOMEND_SEARCH_WORD = 'bread';
-const RECOMEND_PER_PAGE = 30;
+const RECOMEND_PER_PAGE = 10;
 const RECOMEND_QUERY = new URLSearchParams({ ...QUERY, ...{ per_page: `${RECOMEND_PER_PAGE}`, text: `${RECOMEND_SEARCH_WORD}` } });
 const RECOMEND_URL = `${PATH}?${RECOMEND_QUERY}`;
 const createrRecommend = async () => {
     const SELECTOR_RECOMEND = '.js-recommend';
     const recomendTarget = document.querySelector(`${SELECTOR_RECOMEND}`);
-    const photos = await fetchFlickerPhotos(RECOMEND_URL);
-    // <img class="recommend__img" src="${photo.url_h}" alt="${photo.title}">
+    const photos = await fetchFlickrPhotos(RECOMEND_URL);
+  // <img class="recommend__img" src="${photo.url_o}" alt="${photo.title}">
     const thumbnailsImg = photos.map(photo => `
                 <li class="recommend__item">
-                    <img class="recommend__img" src="${photo.url_h}">
+                    ${createLicensedImageTag({
+                        url: `${photo.url_o}`,
+                        alt: `${photo.title}`,
+                        owner: `${photo.owner}`,
+                        id: `${photo.id}`,
+                        type: 'text'
+                    })}
                     <h3 class="recommend__title">${photo.title}</h3>
                     <p class="recommend__price">1,100円(税込み)</p>
                 </li>
