a1ea641aa0018e6c7d1414cdde43399f95b0d804
Shun Nakano <greenpeas.y.man@gmail.com>
2023-11-26 21:18:49 +0900

    リネーム

--- fsm_4/assets/js/fetchFlickrPhotos.js
+++ fsm_4/assets/js/fetchFlickrPhotos.js
@@ -1,9 +1,10 @@
 /**
- * Flicker APIから画像を取得する
+ * Flickr APIから画像を取得する
  * @param url 
  * @returns 
  */
-const fetchFlickerPhotos = async (url) => {
+const fetchFlickrPhotos = async (url) => {
+    console.log('fetchFlickrPhotos');
     const response = await fetch(`${url}`, { mode: 'cors' });
     if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
     const parseResponse = await response.json();
