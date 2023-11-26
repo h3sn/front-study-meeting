b0dc030d16337f6e92666fff9e510a426ae84247
Shun Nakano <greenpeas.y.man@gmail.com>
2023-11-26 13:37:38 +0900

    商品画像に切り替え処理を実装

--- fsm_4/assets/js/top.js
+++ fsm_4/assets/js/top.js
@@ -5,17 +5,23 @@ const GARALLY_PER_PAGE = 10;
 const GARALLY_QUERY = new URLSearchParams({ ...QUERY, ...{ per_page: `${GARALLY_PER_PAGE}`, text: `${GARALLY_SEARCH_WORD}` } });
 const GARALLY_URL = `${PATH}?${GARALLY_QUERY}`;
 const createProductGarally = async () => {
-    let photos = [];
     const SELECTOR_MAIN = '.js-product-garally__main';
     const SELECTOR_THUMBNAILS = '.js-product-garally__thumbnails';
     const mainTarget = document.querySelector(`${SELECTOR_MAIN}`);
     const thumbnailsTarget = document.querySelector(`${SELECTOR_THUMBNAILS}`);
-    photos = await fetchFlickerPhotos(GARALLY_URL);
-    const mainImg = `<img src="${photos[0].url_h}" alt="${photos[0].title}">`;
+    const domParser = new DOMParser();
+    const photos = await fetchFlickerPhotos(GARALLY_URL);
+    const mainImg = domParser.parseFromString(`<img src="${photos[0].url_h}" alt="${photos[0].title}">`, 'text/html').querySelector('img');
     // const thumbnailsImg = photos.map(photo => `<img src="${photo.url_h}" alt="${photo.title}">`).join('');
-    const thumbnailsImg = photos.map(photo => `<img src="${photo.url_h}">`).join('');
-    mainTarget.innerHTML = mainImg;
-    thumbnailsTarget.innerHTML = thumbnailsImg;
+    const thumbnailsImg = domParser.parseFromString(photos.map(photo => `<img src="${photo.url_h}">`).join(''), 'text/html').querySelectorAll('img');
+    thumbnailsImg.forEach(img => {
+        img.addEventListener('click', () => {
+            const src = img.getAttribute('src');
+            mainImg.setAttribute('src', src);
+        });
+        thumbnailsTarget.appendChild(img);
+    });
+    mainTarget.appendChild(mainImg);
 };
 
 // RECOMEND
@@ -24,10 +30,9 @@ const RECOMEND_PER_PAGE = 30;
 const RECOMEND_QUERY = new URLSearchParams({ ...QUERY, ...{ per_page: `${RECOMEND_PER_PAGE}`, text: `${RECOMEND_SEARCH_WORD}` } });
 const RECOMEND_URL = `${PATH}?${RECOMEND_QUERY}`;
 const createrRecommend = async () => {
-    let recommendItems = [];
     const SELECTOR_RECOMEND = '.js-recommend';
     const recomendTarget = document.querySelector(`${SELECTOR_RECOMEND}`);
-    photos = await fetchFlickerPhotos(RECOMEND_URL);
+    const photos = await fetchFlickerPhotos(RECOMEND_URL);
     // <img class="recommend__img" src="${photo.url_h}" alt="${photo.title}">
     const thumbnailsImg = photos.map(photo => `
                 <li class="recommend__item">
